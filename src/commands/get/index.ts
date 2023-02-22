c57ed4207ff491689c1942adee0648a835af9da0
Benjamin Džubur <benjamin.dzubur@zebrabi.com>
2023-02-22 13:32:20 +0100

    Add optional flag to set identity when fetching flags & fix output logs.

--- src/commands/get/index.ts
+++ src/commands/get/index.ts
@@ -1,4 +1,4 @@
-import {Command, Flags} from '@oclif/core'
+import { Command, Flags } from '@oclif/core'
 import fetch from 'node-fetch'
 import flagsmith from 'flagsmith/isomorphic'
 const fs = require('fs')
@@ -10,6 +10,7 @@ export default class FlagsmithGet extends Command {
     '$ flagsmith get <ENVIRONMENT_ID>',
     '$ flagsmith get --o ./my-file.json',
     '$ flagsmith get --a https://flagsmith.example.com/api/v1/',
+    '$ flagsmith get --i Bob',
   ]
 
   static flags = {
@@ -18,6 +19,9 @@ export default class FlagsmithGet extends Command {
     }),
     api: Flags.string({
       char: 'a', description: 'The API URL to fetch the feature flags from', required: false,
+    }),
+    identity: Flags.string({
+      char: 'i', description: 'The identity for which to fetch feature flags', required: false,
     }),
   }
 
@@ -28,20 +32,26 @@ export default class FlagsmithGet extends Command {
   ]
 
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(FlagsmithGet)
+    const { args, flags } = await this.parse(FlagsmithGet)
     const environment = args.environment || process.env.FLAGSMITH_ENVIRONMENT
     const api = flags.api || process.env.FLAGSMITH_ENVIRONMENT
-    if (environment) {
+    if (!environment) {
       this.log('A flagsmith environment was not specified, run flagsmith get --help for more usage.')
     }
-
+    const identity = flags.identity;
+    let outputString = `Flagsmith: Retrieving flags from ${environment}`
+    if (identity) {
+      outputString += ` for identity ${identity}`
+    }
     const output = flags.output
-    this.log(`Flagsmith: Retrieving flags from ${environment}, outputing to ${output}.`)
+    outputString += `, outputing to ${output}.`
+    this.log(outputString)
 
     await flagsmith.init({
       environmentID: environment,
       fetch: fetch,
-      api,
+      api: api,
+      identity: identity,
     })
     fs.writeFileSync(output, JSON.stringify(flagsmith.getState()))
   }
