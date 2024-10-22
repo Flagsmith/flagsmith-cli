f01e9d45bd304e8ca1c48b1cc9a46ad84dbf5685
Rodrigo López Dato <rodrigo.lopezdato@flagsmith.com>
2024-10-22 17:29:24 -0300

    Always hit API URL with trailing slash

--- src/commands/get/index.ts
+++ src/commands/get/index.ts
@@ -2,6 +2,13 @@ import {Command, Flags} from '@oclif/core'
 import fetch from 'node-fetch'
 import flagsmith from 'flagsmith/isomorphic'
 const fs = require('fs')
+
+const withTrailingSlash = (url: string): string => {
+  const parsed = new URL(url)
+  parsed.pathname += parsed.pathname.endsWith('/') ? '' : '/'
+  return parsed.toString()
+}
+
 export default class FlagsmithGet extends Command {
   static description = 'Retrieve flagsmith features from the Flagsmith API and output them to a file.'
 
@@ -21,7 +28,7 @@ export default class FlagsmithGet extends Command {
       char: 'o', description: 'The file path output', required: false, default: './flagsmith.json',
     }),
     api: Flags.string({
-      char: 'a', description: 'The API URL to fetch the feature flags from', required: false,
+      char: 'a', description: 'The API URL to fetch the feature flags from', required: false, default: 'https://edge.api.flagsmith.com/api/v1/'
     }),
     trait: Flags.string({
       char: 't',
@@ -54,7 +61,7 @@ export default class FlagsmithGet extends Command {
   async run(): Promise<void> {
     const {args, flags} = await this.parse(FlagsmithGet)
     const environment = args.environment || process.env.FLAGSMITH_ENVIRONMENT
-    const api = flags.api
+    const api = withTrailingSlash(flags.api)
     if (!environment) {
       this.log('A flagsmith environment was not specified, run flagsmith get --help for more usage.')
     }
@@ -84,7 +91,7 @@ export default class FlagsmithGet extends Command {
     this.log(outputString)
 
     if (isDocument) {
-      fetch(`${api || 'https://edge.api.flagsmith.com/api/v1/'}environment-document/`, {
+      fetch(new URL('environment-document/', api), {
         headers: {
           'x-environment-key': environment,
         },
