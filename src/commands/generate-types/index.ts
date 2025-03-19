08bce2c0d1e8d3bedd697c5aa2dfa02e6dfc5d72
kyle-ssg <kyle@solidstategroup.com>
2025-03-19 14:12:42 +0000

    Add exclude flags

--- src/commands/generate-types/index.ts
+++ src/commands/generate-types/index.ts
@@ -1,7 +1,7 @@
-import {Args, Command, Flags} from '@oclif/core'
+import { Args, Command, Flags } from '@oclif/core'
 import jsonToTypescript from "../../util/json-to-typescript";
 import getAllFeatures from "../../util/get-all-features";
-const fs = require('fs')
+import * as fs from 'fs'
 
 let api = `https://api.flagsmith.com`
 let apiKey = ''
@@ -11,41 +11,61 @@ export default class FlagsmithGenerateTypes extends Command {
   static examples = [
     "export FLAGSMITH_API_KEY=API_KEY flagsmith generate-types PROJECT_ID",
     "export FLAGSMITH_API_KEY=API_KEY flagsmith generate-types PROJECT_ID -a https://selfhosted-flagsmith.example.com",
-    "export FLAGSMITH_API_KEY=API_KEY flagsmith generate-types PROJECT_ID -o ./my-file.d.ts"
+    "export FLAGSMITH_API_KEY=API_KEY flagsmith generate-types PROJECT_ID -o ./my-file.d.ts",
+    "export FLAGSMITH_API_KEY=API_KEY flagsmith generate-types PROJECT_ID -e feature_to_exclude"
   ]
 
   static flags = {
     api: Flags.string({
-      char: 'a', description: 'The API to use if you are self hosted', required: false, default: 'https://api.flagsmith.com',
+      char: 'a',
+      description: 'The API to use if you are self hosted',
+      required: false,
+      default: 'https://api.flagsmith.com',
     }),
     output: Flags.string({
-      char: 'o', description: 'The file path output', required: false, default: './flagsmith.d.ts',
+      char: 'o',
+      description: 'The file path output',
+      required: false,
+      default: './flagsmith.d.ts',
     }),
+    exclude: Flags.string({
+      char: 'e',
+      description: 'Comma separated list of feature names to exclude from type generation to test feature removal',
+      required: false,
+    })
   }
 
   static args = {
     project: Args.string({
-      description:
-        'The flagsmith project id to retrieve the features from',
+      description: 'The flagsmith project id to retrieve the features from',
       required: true
     }),
     output: Args.string({
-      description:
-        'The flagsmith project id to retrieve the features from',
+      description: 'The flagsmith project id to retrieve the features from',
+      required: false,
     }),
   }
 
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(FlagsmithGenerateTypes)
+    const { args, flags } = await this.parse(FlagsmithGenerateTypes)
 
-    if(!process.env.FLAGSMITH_API_KEY) {
+    if (!process.env.FLAGSMITH_API_KEY) {
       throw new Error("FLAGSMITH_API_KEY is not defined.")
     }
-    apiKey = `${process.env.FLAGSMITH_API_KEY}`
+    apiKey = process.env.FLAGSMITH_API_KEY
 
-    //Step 1: get all of the feature flags and environments for the project
-    const features = await getAllFeatures({projectId:args.project, apiKey, api})
-    // Example JSON string
+    let features = await getAllFeatures({ project: `${args.project}`, apiKey, api })
+    const excludeList = flags.exclude?.split(',').map(name => name.trim())
+    if (excludeList?.length) {
+
+      excludeList.map((feature: any) => {
+        console.log("Excluding flag", feature)
+         features.map((environmentFeature)=> {
+          delete environmentFeature[feature]
+        })
+      })
+    }
+
     const jsonString = JSON.stringify(features)
     const output = flags.output
 
--- src/util/get-all-features.ts
+++ src/util/get-all-features.ts
@@ -1,28 +1,27 @@
 import fetch from 'node-fetch'
-import {createFlagsmithInstance} from "flagsmith/isomorphic";
-import {IFlagsmithFeature} from "flagsmith/types";
+import { createFlagsmithInstance } from "flagsmith/isomorphic";
+import { IFlagsmithFeature } from "flagsmith/types";
 import doGet from './doGet'
 
-export default async function (data: {apiKey:string, api: string, project: string }) {
-  const {api,apiKey,project} = data
+export default async function (data: { apiKey: string, api: string, project: string }) {
+  const { api, apiKey, project } = data
   const getReq = doGet(api, apiKey)
-  const environments: {api_key:string, id:number}[] = await getReq(`projects/${project}/environments/`)
-  return  await Promise.all(environments.map((v)=>{
+  const environments: { api_key: string, id: number }[] = await getReq(`projects/${project}/environments/`)
+  return await Promise.all(environments.map((v) => {
     const instance = createFlagsmithInstance()
-    return (
-      // get a key value pair of each feature per environment
-      instance.init({environmentID:v.api_key, fetch}).then(()=>{
-        let features:Record<string, IFlagsmithFeature['value']>  = {}
-        Object.keys(instance.getAllFlags()).map((key)=>{
-          let value = instance.getValue(key)
-          try {
-            //attempt to parse the feature as json to get better types
-            value = JSON.parse(instance.getValue(key))
-          } catch (e) {}
-          features[key] = value
-        })
-        return features
+    return instance.init({ environmentID: v.api_key, fetch }).then(() => {
+      let features: Record<string, IFlagsmithFeature['value']> = {}
+      Object.keys(instance.getAllFlags()).forEach((key) => {
+        let value = instance.getValue(key)
+        try {
+          value = JSON.parse(instance.getValue(key))
+        } catch (e) {}
+        features[key] = value
       })
-    )
+      const sortedFeatures = Object.keys(features)
+        .sort()
+        .reduce((acc, key) => ({ ...acc, [key]: features[key] }), {} as Record<string, IFlagsmithFeature['value']>)
+      return sortedFeatures
+    })
   }))
 }
